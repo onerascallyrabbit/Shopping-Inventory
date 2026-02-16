1134ebe7aaf5db5299cd7d32259701f3e09aa978
onerascallyrabbit <67293258+onerascallyrabbit@users.noreply.github.com>
2026-02-15 16:55:35 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+
+const CACHE_NAME = 'aisle-be-back-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './cart_logo.png',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
